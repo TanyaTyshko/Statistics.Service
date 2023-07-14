2a0ad69738dc0191cce87bb1e6bf96f4492001d9
Tanya <Tanja_33@mail.ru>
2023-07-14 14:34:01 +0700

    Deleted class main

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -28,7 +28,7 @@ class StatisticsServiceTest {
 
         long actual = service.findMax(incomesInBillions);
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);пше 
     }
 }
 
